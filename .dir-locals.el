5fd3e1b08afbf44700c7973bb823be9b0f2d0957
Jorge Ramirez <jorgeramirez927@gmail.com>
2025-12-03 00:37:28 -0500

    job apps and config changes

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (load (expand-file-name "org-notes-config.el"
+                                         default-directory)
+                       t t)))))
---
+++ org-notes-config.el
@@ -0,0 +1,74 @@
+;;; org-notes-config.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 jramirez
+;;
+;; Author: jramirez <jramirez@jrLaptop>
+;; Maintainer: jramirez <jramirez@jrLaptop>
+;; Created: November 29, 2025
+;; Modified: November 29, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/jramirez/org-notes-config
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'org-notes-config)
+;;; org-notes-config.el -*- lexical-binding: t; -*-
+
+;; Optional: derive org-directory from this project root.
+;; If you already set org-directory in $DOOMDIR/config.el, you can skip this.
+(setq org-directory (file-name-as-directory
+                     (expand-file-name "." default-directory)))
+
+;; Org agenda over all org files in this project
+(setq org-agenda-files
+      (directory-files-recursively org-directory "\\.org$"))
+
+;; Org-roam base directory
+(after! org-roam
+  (setq org-roam-directory org-directory)
+
+  ;; Org-roam capture templates
+  (setq org-roam-capture-templates
+        '(("d" "default" plain "%?"
+           :target (file+head "00_INBX/${slug}.org"
+                              "#+title: ${title}\n")
+           :unnarrowed t))
+
+        ;; Roam dailies
+        org-roam-dailies-directory "journals/"
+        org-roam-dailies-capture-templates
+        '(("d" "default" entry "* %?"
+           :if-new (file+head "%<%Y-%m-%d>.org"
+                              "#+title: %<%a - %b %d>\n")))))
+
+;; Org capture templates
+(after! org
+  (setq org-capture-templates
+        '(("t" "Todos Entries")
+          ("tt" "Todo" entry
+           (file+headline "00_INBX/inbox.org" "Tasks")
+           "* TODO %?\n  %i\n  %a")
+          ("td" "Daily Todo" entry
+           (file+olp+datetree "02_AREA/journal.org")
+           "* [ ] %?\n  %i\n  %a")
+
+          ("j" "Jornal Entries")
+          ("jj" "Journal" entry
+           (file+olp+datetree "02_AREA/journal.org")
+           "* %U %?\n%i\n%a" :prepend t)
+
+          ("jf" "Log Food" entry
+           (file+olp+datetree "02_AREA/food_log.org")
+           "* %U %?\n" :prepend t))))
+
+;;; org-notes-config.el ends here
